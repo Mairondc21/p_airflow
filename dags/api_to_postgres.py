d7b90034730dfd67301a3d7daef2feb200b16ba9
Mairondc21 <duartemairon@gmail.com>
2024-10-17 12:15:13 -0300

    includes adicionados e nova DAG inicializada

---
+++ dags/api_to_postgres.py
@@ -0,0 +1,33 @@
+from airflow.decorators import dag, task
+from datetime import datetime
+from include.controller import gerar_numero_aleatorio, add_pokemon_to_db, fetch_pokemon_data
+
+@dag(
+    dag_id='api_postgres',
+    description="pipeline_para_capturar_pokemon",
+    start_date=datetime(2024,3,24),
+    schedule="* * * * *",
+    catchup=False
+)
+def api_postgres():
+    
+    @task(task_id='gerar_numero_aleatorio')
+    def task_gerar_numero_aleatorio():    
+        return gerar_numero_aleatorio()
+
+    @task(task_id='fetch_pokemon_data')
+    def task_fetch_pokemon_data():    
+        return fetch_pokemon_data() 
+
+    @task(task_id='add_pokemon_to_db')
+    def task_add_pokemon_to_db():    
+        return add_pokemon_to_db()
+
+    t1 = task_gerar_numero_aleatorio()
+    t2 = task_fetch_pokemon_data(t1)
+    t3 = task_add_pokemon_to_db(t2)
+
+    t1 >> t2 >> t3
+
+api_postgres()
+
