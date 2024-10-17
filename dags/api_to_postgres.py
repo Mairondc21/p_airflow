0cf473c57fe82f7f404d9b8fced7db2beb380b51
Mairondc21 <duartemairon@gmail.com>
2024-10-17 12:18:51 -0300

    update

--- dags/api_to_postgres.py
+++ dags/api_to_postgres.py
@@ -16,12 +16,12 @@ def api_postgres():
         return gerar_numero_aleatorio()
 
     @task(task_id='fetch_pokemon_data')
-    def task_fetch_pokemon_data():    
-        return fetch_pokemon_data() 
+    def task_fetch_pokemon_data(numero_aleatorio):    
+        return fetch_pokemon_data(numero_aleatorio) 
 
     @task(task_id='add_pokemon_to_db')
-    def task_add_pokemon_to_db():    
-        return add_pokemon_to_db()
+    def task_add_pokemon_to_db(pokemon_data):    
+        add_pokemon_to_db(pokemon_data)
 
     t1 = task_gerar_numero_aleatorio()
     t2 = task_fetch_pokemon_data(t1)
